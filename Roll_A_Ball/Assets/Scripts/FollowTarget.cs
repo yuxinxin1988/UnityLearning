e6656481d1bf404cec1e3af112cc0d57ce19278f
yu.xinxin <yu.xinxin@dena.jp>
2016-03-31 20:24:15 +0800

    roll_a_ball project

---
+++ Roll_A_Ball/Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform playerTrans;
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - playerTrans.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTrans.position + offset;
+	}
+}
---
+++ Roll_A_Ball/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody body;
+    public int force = 5;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        body.AddForce(new Vector3(h, 0, v)*force);
+        //float step = 0.2f;
+        //Vector3 orgPos = transform.localPosition;
+        //if (Input.GetKey(KeyCode.A))
+        //    transform.localPosition = new Vector3(orgPos.x - step, orgPos.y, orgPos.z);
+
+        //if (Input.GetKey(KeyCode.D))
+        //    transform.localPosition = new Vector3(orgPos.x + step, orgPos.y, orgPos.z);
+	}
+}
